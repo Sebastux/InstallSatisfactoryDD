3034aadf5d7dc58a1ff58efde0e4e13ebc319a08
Sebastux59 <tata@tata.com>
2025-11-09 19:58:25 +0100

    Améliore la clarté du message de configuration
    Déplace le message de fin pour mettre en évidence les avertissements importants avant la conclusion.
    Ajuste le chemin du fichier './ficscommun/satis' pour une meilleure robustesse.
    Met à jour la date de modification interne du script.

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@
 # Description      : Prépare l'éxécution du role ansible.
 # Auteur           : Sébastux
 # Date             : 08/11/2025
-# Modification     : 08/11/2025
+# Modification     : 09/11/2025
 # Version          : 1.00
 # Utilisation      : config.sh
 # Notes            : Script permettant l'installation de satisfactory dedicated server.
@@ -22,14 +22,14 @@ apt-get install -y aptitude python3 python3-pip python3-apt python3-venv build-e
 
 # Affichage de divers informations
 echo "***************************************************************"
-cat ficscommun/satis
+cat ./ficscommun/satis
 echo "Pour installer ansible, utilisez la commande make install."
 echo "Pour installer le serveur satisfactory, utilisez la commande make run."
 echo "Pour avoir plus de commande, utilisez la commande make."
-echo "Bon jeux."
-echo ""
 echo ""
 echo "ATTENTION :"
 echo "Avant de lancer le serveur, pensez à éditer le fichier production/inventory pour configurer"
 echo "l'adresse IP à mettre en liste blanche pour fail2ban ainsi que le mot de passe du compte applicatif."
 echo "Consultez le README.md pour plus d'informations."
+echo ""
+echo "Bon jeux."
